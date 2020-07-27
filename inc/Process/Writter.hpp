9ea74ef9c8a62704386279cfadbeb9bbdd326eb2
Amaya <guillaume.le-berre@epitech.eu>
2020-07-27 02:08:46 +0200

    [ADD] Writter

---
+++ inc/Process/Writter.hpp
@@ -0,0 +1,21 @@
+/*
+** PERSONAL PROJECT, 2020
+** UWU
+** File description:
+** Writter
+*/
+
+#pragma once
+
+namespace Process {
+
+    class Writter {
+
+        public:
+
+            Writter() = default;
+            ~Writter() = default;
+
+    };
+
+}
---
+++ src/Process/Writter.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2020
+** UWU
+** File description:
+** Writter
+*/
+
+#include "Writter.hpp"
+
+namespace Process {
+
+}
