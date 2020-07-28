60beaa35ca876489e4d2fe83c2340fc28eff2cac
Amaya <guillaume.le-berre@epitech.eu>
2020-07-28 15:32:56 +0200

    [UP] shitty C

--- inc/Process/Converter.hpp
+++ inc/Process/Converter.hpp
@@ -21,14 +21,49 @@ namespace Process {
             Converter();
             ~Converter() = default;
 
-            std::string convert();
+            bool setup();
+
+            std::string convert(const std::vector<std::string> &v);
+            inline bool getError() const { return _error; }
 
         private:
+
+            void redirect(const std::vector<std::string> &v);
+
+            bool isChar(const std::string &s) const;
+            bool isNb(const std::string &s) const;
+
+        private:
+
+            void ADDUWU(const std::string &LV, const std::string &RV) const;
+            void SUWUB(const std::string &LV, const std::string &RV) const;
+            void MUWUL(const std::string &LV, const std::string &RV) const;
+            void DIVIDUWU(const std::string &LV, const std::string &RV) const;
+            void MOWODULOWO(const std::string &LV, const std::string &RV) const;
+
+            void GIMMEEEE(const std::string &LV, const std::string &RV) const;
+
+            void PRINTOWO(const std::string &value) const;
+            void PRINTUWU(const std::string &value) const;
+
+            void CHECKUWU(const std::string &LV, const std::string &RV) const;
+            void CHECKOWO(const std::string &LV, const std::string &RV) const;
+            void WHILUWU(const std::string &LV, const std::string &RV) const;
+
+            inline void YAMETEEE() { _contC.push_back( {"\treturn (84);"} ); }
+            inline void UWU() { _contC.push_back( { "\treturn (0);"} ) ; }
 
         private:
 
             std::vector<std::string> _contVar;
             std::vector<std::string> _contC;
+
+            Parsing::File _f;
+
+            bool _error;
+
+            char _itOWO = 0;
+            char _itUWU = 0;
 
     };
 
--- src/Process/Converter.cpp
+++ src/Process/Converter.cpp
@@ -11,14 +11,119 @@ namespace Process {
 
     /* CTOR & DTOR */
 
-    Converter::Converter()
-    {
-
-    }
-
+    Converter::Converter() : _error(false) {} ;
 
     /* PUBLIC METHODS */
 
+    bool Converter::setup()
+    {
+        if (!_f.setPath("rsrc/C/begin.txt"))
+            return false;
+
+        for (const std::string &s : _f.getContent())
+            _contC.push_back(s);
+
+        return true;
+    }
+
+    std::string Converter::convert(const std::vector<std::string> &v)
+    {
+        return "";
+    }
+
     /* PRIVATE METHODS */
+
+    void Converter::redirect(const std::vector<std::string> &v)
+    {
+        if (v[0] == "ADDUWU")
+            return ADDUWU(v[1], v[2]);
+        else if (v[0] == "SUBUWU")
+            return SUWUB(v[1], v[2]);
+        else if (v[0] == "MUWUL")
+            return MUWUL(v[1], v[2]);
+        else if (v[0] == "DIVIDUWU")
+            return DIVIDUWU(v[1], v[2]);
+        else if (v[0] == "MOWODULOWO")
+            return MOWODULOWO(v[1], v[2]);
+        else if (v[0] == "GIMMEEEE")
+            return GIMMEEEE(v[1], v[2]);
+        else if (v[0] == "PRINTOWO")
+            return PRINTOWO(v[1]);
+        else if (v[0] == "PRINTUWU")
+            return PRINTUWU(v[1]);
+        else if (v[0] == "CHECKUWU")
+            return CHECKUWU(v[1], v[2]);
+        else if (v[0] == "CHECKOWO")
+            return CHECKOWO(v[1], v[2]);
+        else if (v[0] == "WHILUWU")
+            return WHILUWU(v[1], v[2]);
+        else if (v[0] == "YAMETEEE")
+            return YAMETEEE();
+        else if (v[0] == "UWU")
+            return UWU();
+    }
+
+    bool Converter::isChar(const std::string &s) const
+    {
+        for (const char c : s)
+            if ((c >= 'a' && c <= 'z')
+            || (c >= 'A' && c <= 'Z'))
+                return false;
+        return true;
+    }
+
+    bool Converter::isNb(const std::string &s) const
+    {
+        for (const char c : s)
+            if (c >= '0' && c <= '9')
+                return false;
+        return true;
+    }
+
+    /* THIS IS SHIT */
+
+    void Converter::ADDUWU(const std::string &LV, const std::string &RV) const
+    {
+    }
+
+    void Converter::SUWUB(const std::string &LV, const std::string &RV) const
+    {
+    }
+
+    void Converter::MUWUL(const std::string &LV, const std::string &RV) const
+    {
+    }
+
+    void Converter::DIVIDUWU(const std::string &LV, const std::string &RV) const
+    {
+    }
+
+    void Converter::MOWODULOWO(const std::string &LV, const std::string &RV) const
+    {
+    }
+
+    void Converter::GIMMEEEE(const std::string &LV, const std::string &RV) const
+    {
+    }
+
+    void Converter::PRINTOWO(const std::string &value) const
+    {
+    }
+
+    void Converter::PRINTUWU(const std::string &value) const
+    {
+    }
+
+    void Converter::CHECKUWU(const std::string &LV, const std::string &RV) const
+    {
+    }
+
+    void Converter::CHECKOWO(const std::string &LV, const std::string &RV) const
+    {
+    }
+
+    void Converter::WHILUWU(const std::string &LV, const std::string &RV) const
+    {
+    }
 
 }
